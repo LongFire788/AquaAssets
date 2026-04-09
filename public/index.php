616e9b07c933eedae60131f57cebdd3d9f626e82
LongFire788 <117385528+LongFire788@users.noreply.github.com>
2026-04-09 19:34:14 -0300

    docs: retirando comentarios

--- public/index.php
+++ public/index.php
@@ -10,16 +10,14 @@ $dotenv->safeLoad();
 
 $templates = new Engine(__DIR__ . '/../src/Views');
 
-// Pega a URL que o usuário digitou (ex: /credits)
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-// Lógica de Roteamento Simples
 if ($uri === '/' || $uri === '/home') {
     echo $templates->render('home');
 } elseif ($uri === '/credits') {
     echo $templates->render('credits');
 } else {
-    // Página 404 personalizada se quiser
+
     http_response_code(404);
     echo "Página não encontrada!";
 }
