93f8caa5adb92d8465c18348db6f2f4e3a68c50e
LongFire788 <117385528+LongFire788@users.noreply.github.com>
2026-04-09 19:03:45 -0300

    docs: retirando comentarios desnecessarios

--- public/index.php
+++ public/index.php
@@ -5,14 +5,10 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Dotenv\Dotenv;
 use League\Plates\Engine;
 
-// 1. Carrega as variáveis de ambiente (.env)
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->safeLoad();
 
-// 2. Inicializa o motor de templates (Plates)
-// Apontamos para a pasta src/Views onde estarão nossos arquivos .php
+
 $templates = new Engine(__DIR__ . '/../src/Views');
 
-// 3. Renderiza a Landing Page (home.php)
-// O Plates vai procurar por um arquivo chamado 'home.php' em src/Views
 echo $templates->render('home');
